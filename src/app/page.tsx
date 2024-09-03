817f355590b55ad2d5d8aec385de0d78502d3c9a
Ansh Gujral <anshgujral1997@gmail.com>
2024-09-04 01:06:15 +0530

    fix use client crap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,79 +1,14 @@
-"use client";
-
 import React, { useState, useEffect } from "react";
-import Link from "next/link";
-import styles from "./page.module.scss";
 import { Metadata } from "next";
+import Home from "@/components/home/home";
 
 export async function generateMetadata(): Promise<Metadata> {
-	return {
-	  title: 'Ansh Gujral',
-	  description: 'Coming Soon...'
-	}
-  }
+  return {
+    title: "Ansh Gujral",
+    description: "Coming Soon...",
+  };
+}
 
 export default function RetroComingSoon() {
-  const [text, setText] = useState("");
-  const fullText =
-    "> INITIALIZING PORTFOLIO...\n> LOADING MODULES...\n> COMPILING EXPERIENCE...\n> DEBUGGING SKILLS...\n> COMING SOON...";
-
-  useEffect(() => {
-    let index = 0;
-    const intervalId = setInterval(() => {
-      setText((prevText) => {
-        if (index < fullText.length) {
-          index++;
-          return fullText.slice(0, index);
-        }
-        clearInterval(intervalId);
-        return prevText;
-      });
-    }, 50);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <div className={styles.terminal}>
-          <pre className={styles.pre}>
-            {text}
-            <span className={styles.cursor}>_</span>
-          </pre>
-        </div>
-
-        <h1 className={styles.title}>COMING SOON</h1>
-        <p className={styles.description}>
-          My cutting-edge software engineering portfolio is currently in
-          development. Stay tuned for the launch!
-        </p>
-
-        <div className={styles.links}>
-          <Link
-            href="https://github.com/gujral1997"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            [GITHUB]
-          </Link>
-          <Link
-            href="https://www.linkedin.com/in/gujral1997"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            [LINKEDIN]
-          </Link>
-          <Link
-            href="https://x.com/gujral1997"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            [TWITTER]
-          </Link>
-          <Link href="mailto:anshgujral1997@gmail.com">[EMAIL]</Link>
-        </div>
-      </main>
-    </div>
-  );
+  return <Home />;
 }
---
+++ src/components/home/home.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import Link from "next/link";
+import { FC, useEffect, useState } from "react";
+import styles from "./home.module.scss";
+
+const Home: FC = () => {
+  const [text, setText] = useState("");
+  const fullText =
+    "> INITIALIZING PORTFOLIO...\n> LOADING MODULES...\n> COMPILING EXPERIENCE...\n> DEBUGGING SKILLS...\n> COMING SOON...";
+
+  useEffect(() => {
+    let index = 0;
+    const intervalId = setInterval(() => {
+      setText((prevText) => {
+        if (index < fullText.length) {
+          index++;
+          return fullText.slice(0, index);
+        }
+        clearInterval(intervalId);
+        return prevText;
+      });
+    }, 50);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div className={styles.terminal}>
+          <pre className={styles.pre}>
+            {text}
+            <span className={styles.cursor}>_</span>
+          </pre>
+        </div>
+
+        <h1 className={styles.title}>COMING SOON</h1>
+        <p className={styles.description}>
+          My cutting-edge software engineering portfolio is currently in
+          development. Stay tuned for the launch!
+        </p>
+
+        <div className={styles.links}>
+          <Link
+            href="https://github.com/gujral1997"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            [GITHUB]
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/gujral1997"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            [LINKEDIN]
+          </Link>
+          <Link
+            href="https://x.com/gujral1997"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            [TWITTER]
+          </Link>
+          <Link href="mailto:anshgujral1997@gmail.com">[EMAIL]</Link>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
